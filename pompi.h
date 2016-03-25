650e5948e37d12c75bdfde6104689452115ec8a2
Lukas Slouka <xslouk02@stud.fit.vutbr.cz>
2016-03-25 13:49:34 +0100

    Papi events added (fix #2)

--- pompi.h
+++ pompi.h
@@ -5,7 +5,9 @@
 #include <vector>
 #include <string>
 #include <iostream>
-
+#include <stdio.h>
+#include <cstring>
+#include <algorithm>
 
 namespace pompi
 {
@@ -20,12 +22,16 @@ namespace pompi
             double execution_time_start_;
             double execution_time_end_;
             std::vector< int > papi_events_;
+            std::vector< std::string > papi_event_names_;
             std::vector< std::vector< long long > > thread_data_;
             int max_threads_;
 
 
         public:
+
             Base();
+
+            void AddEvent(char * event);
     };
 
     // Implementation
@@ -33,27 +39,53 @@ namespace pompi
     Base::Base()
     {
         max_threads_ = omp_get_max_threads();
-        std::clog << "Maximum number of threads is: " << max_threads_ << std::endl;
+        std::clog << "[Log] Maximum number of threads is " << max_threads_ << std::endl;
 
         thread_data_.resize(max_threads_);
 
-        int PAPI_return_value;
-
-        PAPI_return_value = PAPI_library_init(PAPI_VER_CURRENT);
+        int PAPI_return_value = PAPI_library_init(PAPI_VER_CURRENT);
         if(PAPI_return_value != PAPI_VER_CURRENT)
         {
-            std::cerr << "Could not initialize PAPI library" << std::endl;
+            std::cerr << "[Error] Could not initialize PAPI library" << std::endl;
             exit(1);
         }
-        std::clog << "PAPI library successfully initialized" << std::endl;
+        std::clog << "[Log] PAPI library successfully initialized" << std::endl;
 
         PAPI_return_value = PAPI_thread_init((unsigned long (*)(void)) (omp_get_thread_num));
-        if (PAPI_return_value != PAPI_OK)
+        if(PAPI_return_value != PAPI_OK)
         {
-            std::cerr << "Coult not initialize OpenMP threading for PAPI" << std::endl;
+            std::cerr << "[Error] Coult not initialize OpenMP threading for PAPI" << std::endl;
             exit(1);
         }
-        std::clog << "OpenMP threading for PAPI successfully initialized" << std::endl;
+        std::clog << "[Log] OpenMP threading for PAPI successfully initialized" << std::endl;
+
+        // Resolving PAPI events from PAPI_EVENTS enviroment variable
+        char * papi_events = getenv("PAPI_EVENTS");
+        char * event = strtok(papi_events, "|");
+        int event_id;
+        while(event != NULL)
+        {
+            AddEvent(event);
+            event = strtok(NULL, "|");
+        }
     }
 
+    void Base::AddEvent(char * event)
+    {
+        int event_id;
+        int PAPI_return_value = PAPI_event_name_to_code(event, &event_id);
+        if(PAPI_return_value != PAPI_OK)
+            std::cerr << "[Warning] Papi event `" << event << "` does not exists, skipping" << std::endl;
+        else
+        {
+            if(std::find(papi_events_.begin(), papi_events_.end(), event_id) == papi_events_.end())
+            {
+              papi_event_names_.push_back(std::string(event));
+              papi_events_.push_back(event_id);
+              std::clog << "[Log] Adding papi event `" << event << "`" << std::endl;
+            }
+            else
+                std::cerr << "[Warning] Papi event `" << event << "` already listed, skipping" << std::endl;
+        }
+    }
 }
